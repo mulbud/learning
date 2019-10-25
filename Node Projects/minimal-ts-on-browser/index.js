2c3002c4272ad2d735f7c931d62e6d2333e28609
Muljadi Budiman <mul@radialogica.com>
2019-10-25 14:04:36 -0500

    Added project to demonstrate running a SINGLE TS file on browser - minimal solution

---
+++ Node Projects/minimal-ts-on-browser/index.js
@@ -0,0 +1,4 @@
+"use strict";
+document.getElementById("buttonFirst").addEventListener("click", function () {
+    alert("Test");
+});
---
+++ Node Projects/minimal-ts-on-browser/index.tsx
@@ -0,0 +1,5 @@
+
+document.getElementById("buttonFirst")!.addEventListener("click", function() {
+    alert("Test");
+});
+
